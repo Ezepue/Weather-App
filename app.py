899da87a25c4a13682d216a67ce7419aa0aeade5
Ezepue <ebukaezepue@gmail.com>
2025-02-13 12:34:20 +0100

    Added Forecast

--- app.py
+++ app.py
@@ -4,21 +4,32 @@ import requests
 app = Flask(__name__)
 
 API_KEY = "7e9f9ec3865241c187795412251302"
-BASE_URL = "http://api.weatherapi.com/v1/current.json"
+BASE_URL = "http://api.weatherapi.com/v1/"
 
+
+def get_weather(city):
+    url = f"{BASE_URL}current.json?key={API_KEY}&q={city}&aqi=yes"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    return None
+
+def get_forecast(city):
+    url = f"{BASE_URL}forecast.json?key={API_KEY}&q={city}&days=3&aqi=yes&alerts=yes"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    return None   
+    
 @app.route("/", methods=["GET", "POST"])
 def home():
     weather_data = None
+    forecast_data = None
     if request.method == "POST":
         city = request.form["city"]
-        url = f"{BASE_URL}?key={API_KEY}&q={city}"
-        response = requests.get(url)
-        if response.status_code == 200:
-            weather_data = response.json()
-        else:
-            weather_data= {"error": "City not found"}
-    
-    return render_template("index.html", weather=weather_data)
+        weather_data = get_weather(city)
+        forecast_data = get_forecast(city) 
+    return render_template("index.html", weather=weather_data, forecast=forecast_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
